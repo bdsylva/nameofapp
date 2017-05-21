d3f3e49a12af2f1db01648286e2b185ea1e79f6b
Bethany D'Sylva <bethany.dsylva@students.ebs.edu>
2017-05-21 12:31:04 +0000

    nav toggle fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require rails-ujs
+//= require jquery_ujs
 //= require turbolinks
-//= require_tree . 
+//= require_tree .
+//= require bootstrap
+('.dropdown-toggle').dropdown();
